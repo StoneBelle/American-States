d50fceaa1f672b030367a9bbe61b14cf1fa12f26
Stone <65476500+StoneBelle@users.noreply.github.com>
2022-05-24 22:35:16 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ user_prompt = "Enter a State"
 check_answer = Answers()
 
 while len(score.valid_answers) < 50:
-    answer = wn.textinput(title="Guess the States", prompt=user_prompt).title()
+    answer = wn.textinput(title="Guess the States", prompt=user_prompt).title().strip()
     # If user exits from game early, store all the states that have not been named in a new CSV file.
     if answer == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in score.valid_answers:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in score.valid_answers]
+        # for state in all_states:
+        #     if state not in score.valid_answers:
+        #         missing_states.append(state)
         df = pd.DataFrame(missing_states)  # Converts "missing_states" list into a data frame called "df"
         df.to_csv("missing_states.csv")  # Converts and save "df" into a csv file within directory
         break  # Exits program early (i.e. closes pop up window). Thus, program does not need "wn.mainloop"
